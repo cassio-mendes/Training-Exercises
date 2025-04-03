ec527b48edeb46b37e7f5df901e8bab025963408
cassio-mendes <133818577+cassio-mendes@users.noreply.github.com>
2025-04-03 19:54:59 -0300

    Third Exercise
    Recieves two numbers and print their product

---
+++ Begginer/VariableProduct.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class VariableProduct {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        
+        System.out.println("Product = " + (number1 * number2));
+    }
+    
+}
